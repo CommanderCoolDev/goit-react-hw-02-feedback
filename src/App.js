909b4e32d399cb5f72a72cf28ebb0d37720860b5
Dima <78321515+CommanderCoolDev@users.noreply.github.com>
2021-09-30 10:52:07 +0300

    added  function on btn

--- src/App.js
+++ src/App.js
@@ -13,12 +13,25 @@ export default class App extends Component {
     neutral: 0,
     bad: 0,
   };
+
+  addFeedback = e => {
+    this.setState(prevState => ({ [e]: prevState[e] + 1 }));
+  };
+
   render() {
     return (
       <div>
         <h2>Please leave Feedback</h2>
         {Object.keys(this.state).map(i => (
-          <button className="">{i}</button>
+          <button
+            key={i}
+            className=""
+            onClick={() => {
+              this.addFeedback(i);
+            }}
+          >
+            {i}
+          </button>
         ))}
         <h2>Statistics</h2>
         {Object.keys(this.state).map(i => (
